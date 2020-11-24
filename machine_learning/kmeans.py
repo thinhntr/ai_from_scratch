8b05d49b6b1ee1e2bd1108557c9868059cf9e2a5
Thinh Nguyen <thinhntr@gmail.com>
2020-11-25 05:53:53 +0700

    Backup

--- machine_learning/kmeans.py
+++ machine_learning/kmeans.py
@@ -1,17 +1,23 @@
 import numpy as np
 from collections import Counter
 
+
 def check_input(X):
-    assert(isinstance(X, np.ndarray))
-    assert(len(X.shape) == 2)
+    assert (isinstance(X, np.ndarray))
+    assert (len(X.shape) == 2)
 
 
-rg = np.random.default_rng()
+# Only use for debugging
+rg = np.random.default_rng(12)
 
 
 class KMeans:
     @property
     def k(self):
+        """
+        :return:
+        Number of clusters
+        """
         if self.centroids is None:
             raise RuntimeError("fit() hasn't run yet")
         return len(self.centroids)
@@ -22,23 +28,23 @@ class KMeans:
 
     def fit(self, X, k):
         check_input(X)
-        assert(isinstance(k, int))
+        assert (isinstance(k, int))
 
-        self.centroids = rg.choice(X, k, replace=False)
+        self.centroids = rg.choice(X, k, replace=False).tolist()
 
         for _ in range(self.n_iters):
             y = self.predict(X)
 
-            for label in range(self.centroids.shape[0]):
+            for label in range(len(self.centroids)):
                 points_in_cluster = X[y == label, :]
-                n_points = points_in_cluster.shape[0]
-                self.centroids[label] = (1/n_points) * points_in_cluster.mean(axis=0)
-
+                if points_in_cluster.any():
+                    self.centroids[label] = points_in_cluster.mean(axis=0)
+        self.centroids = np.array(self.centroids)
         return self
 
     def predict(self, X):
         check_input(X)
-        assert(self.centroids is not None)
+        assert (self.centroids is not None)
 
         n_examples = X.shape[0]
         n_features = X.shape[1]
@@ -47,17 +53,18 @@ class KMeans:
         new_X = np.hstack([X, padding]).T
 
         base = np.tile(np.identity(n_features), (self.k, 1))
-        T = np.hstack([base, -self.centroids.ravel().reshape(-1, 1)])
+        T = np.hstack([base, -np.array(self.centroids).ravel().reshape(-1, 1)])
 
-        tmp = (T @ new_X).reshape(n_features, n_examples * self.k)
+        tmp = T @ new_X
+        tmp = np.hstack(np.split(tmp, n_features))
         distances = np.linalg.norm(tmp, axis=0)
 
         # each example lie in 1 column
         # each row i represents the distance from example in column j to cluster i
-        distances_per_example = distances.reshape(self.k, n_examples) 
+        distances_per_example = distances.reshape(n_features, n_examples)
 
         # find closest cluster for each example
         y = np.argmin(distances_per_example, axis=0)
 
-        return y
 
+        return y
--- test.py
+++ test.py
@@ -3,16 +3,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from machine_learning.kmeans import KMeans
+from sklearn import cluster
 
+if __name__ == '__main__':
+    # Setup test
+    rg = np.random.default_rng(29)
+    points = rg.random(size=(150, 3))
 
-rg = np.random.default_rng(12)
+    # Model config
+    n_iters = 50
+    k = 4
 
-points = rg.integers(-10, 10, size=(5, 2))
+    # My model
+    model = KMeans(n_iters=n_iters)
+    model.fit(points, k)
+    my_centers = model.centroids
+    print(my_centers)
 
-model = KMeans(n_iters=20).fit(points, 2)
-centers = model.centroids
-print(centers)
+    # sklearn model
+    sk_model = cluster.KMeans(k)
+    sk_model.fit(points)
+    sk_centers = sk_model.cluster_centers_
+    print(sk_centers)
 
-plt.scatter(points[:, 0], points[:, 1])
-plt.scatter(centers[:, 0], centers[:, 1])
-plt.show()
+    print(((sk_centers - my_centers) ** 2).sum() / (2 * sk_centers.size))
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(points[:, 0], points[:, 1], points[:, 2])
+    ax.scatter(my_centers[:, 0], my_centers[:, 1], my_centers[:, 2], color='green')
+    ax.scatter(sk_centers[:, 0], sk_centers[:, 1], sk_centers[:, 2], color='red')
+    plt.show()
