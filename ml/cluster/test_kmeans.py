eabbf78dbe4ce45beba3f2c6b5d69b6aa626063d
thinhntr <thinhntr@gmail.com>
2021-01-06 22:47:38 +0700

    Refactor

--- ml/cluster/test_kmeans.py
+++ ml/cluster/test_kmeans.py
@@ -1,13 +1,14 @@
-from .kmeans import KMeans
+from .kmeans import KMeans as MyKMeans
 import numpy as np
 from sklearn.datasets import make_blobs
 
 
 class TestKMeansClass:
     def test_predict1(self):
-        model = KMeans()
         n_samples = 1500
         n_clusters = 5
+
+        # Expected result
         X, _, centers = make_blobs(n_samples=n_samples,
                                    centers=n_clusters,
                                    random_state=29,
@@ -17,13 +18,13 @@ class TestKMeansClass:
             for j, center in enumerate(centers):
                 distances[i][j] = np.linalg.norm(x - center)
         y = np.argmin(distances, axis=1)
-
+        # Actual result
+        model = MyKMeans(n_clusters)
         model.centroids = centers
         my_predict = model.predict(X)
 
+        # Test
         assert my_predict.shape == y.shape
         assert my_predict.dtype == np.int
-        print(my_predict)
-        print(y)
         assert (my_predict == y).all()
 
