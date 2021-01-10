373d707a726dd53ab94e45d9829cc77393e064e3
thinhntr <thinhntr@gmail.com>
2021-01-10 23:55:54 +0700

    Add tests for linear_regression

---
+++ ml/linear_model/linear_regression/test_linear_regression.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+from .linear_regression import LinearRegression
+
+
+class TestLinearRegression:
+    def test_predict(self):
+        # random input
+        n_samples = 10
+        n_features = 15
+        rg = np.random.default_rng()
+        X = rg.integers(-5, 6, (n_samples, n_features))
+
+        # expected model
+        expected_W = rg.integers(-10, 21, n_features)
+        expected_b = 10
+
+        # expected output
+        expected_y = expected_W @ X.T + expected_b
+
+        a = LinearRegression()
+        a.W = expected_W
+        a.b = expected_b
+
+        assert (a.predict(X) == expected_y).all()
+
+    def test_fit1(self):
+        X = np.array([[0, 0],
+                      [1, 1]])
+        y = np.array([1, 4])
+
+        expected_W = np.array([1, 1])
+        expected_b = 2
+
+        model = LinearRegression()
+        model.fit(X, y)
+        y_hat = model.predict(X)
+
+        assert (y_hat == y).all()
+        assert (model.W == expected_W).all()
+        assert (model.b == expected_b).all()
+
